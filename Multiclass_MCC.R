32e631be01f19c15b611fa594f9dc533a590ac41
marcabrus <marcabrus@gmail.com>
2019-01-25 09:54:33 +0100

    Update Multiclass_MCC.R
    updated with the correct publication, added some notes

--- Multiclass_MCC.R
+++ Multiclass_MCC.R
@@ -1,10 +1,9 @@
 # v. 0.1
 # 10/01/2018
 #
-#' Calculates MCC also for Multiclass models. The formula is the same reported in the following article:
-#' Jurman, Giuseppe, Samantha Riccadonna, and Cesare Furlanello. 
-#' "A comparison of MCC and CEN error measures in multi-class prediction." PloS one 7.8 (2012): e41882.
-#' 
+#' Gorodkin, Jan. "Comparing two K-category assignments by a K-category correlation coefficient."
+#' Computational biology and chemistry 28.5-6 (2004): 367-374.
+#'
 #' It takes two factors, one for the observed values one for the predicted
 #'
 #' @param obs factor with observed values
@@ -14,17 +13,20 @@
 #'
 
 multi_MCC <- function(obs,pred) {
+  # create the contincency table with the r base function table
   cont_matr<-as.data.frame.matrix(table(pred, obs))
+  # not necessary, change names to contingency table
   colnames(cont_matr)<-paste(colnames(cont_matr), "Ref", sep = "_")
   rownames(cont_matr)<-paste(rownames(cont_matr), "Pred", sep = "_")
+
+  #calculate covariance (obs,pred)
   covxy = 0
   for (k in 1:length(cont_matr)[1]) {
     for (l in 1:length(cont_matr)[1])
       for (m in 1:length(cont_matr)[1])
-        covxy <-
-          ((cont_matr[k, k] * cont_matr[m, l]) - (cont_matr[l, k] * cont_matr[k, m])) + covxy
+        covxy <-  ((cont_matr[k, k] * cont_matr[m, l]) - (cont_matr[l, k] * cont_matr[k, m])) + covxy
   }
-  
+  #calculate covariance (obs,pred)
   covxx = 0
   for (k in 1:length(cont_matr)[1]) {
     covxxa = 0
@@ -36,16 +38,17 @@ multi_MCC <- function(obs,pred) {
       if (f == k) {
         next
       }
-      
+
       for (g in 1:length(cont_matr)[1]) {
         covxxb <- covxxb + cont_matr[g, f]
       }
-      
+
     }
-    
+
     covxx <- covxx + (covxxa * covxxb)
   }
-  
+
+  #calculate covariance (pred, pred)
   covyy = 0
   for (k in 1:length(cont_matr)[1]) {
     covyya = 0
@@ -57,18 +60,18 @@ multi_MCC <- function(obs,pred) {
       if (f == k) {
         next
       }
-      
+
       for (g in 1:length(cont_matr)[1]) {
         covyyb <- covyyb + cont_matr[f, g]
       }
-      
+
     }
-    
+
     covyy <- covyy + (covyya * covyyb)
   }
 
   MCC <- covxy / (sqrt(covxx) * sqrt(covyy))
-#print MCC  
+#print MCC
   message(" ")
   cat(paste(" MCC : ",format(MCC,digits=2)))
 return(MCC)
